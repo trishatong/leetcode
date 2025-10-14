b6d4dad959a8fcda81b40ad06661d57190685591
Trisha Tong <trtong1@gmail.com>
2025-10-14 16:21:47 -0700

    69. Sqrt(x)

---
+++ math/Sqrtx_69.java
@@ -0,0 +1,24 @@
+// 69. Sqrt(x)
+/**
+ * Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
+ * The returned integer should be non-negative as well.
+ * You must not use any built-in exponent function or operator.
+ * For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ */
+
+class Solution {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if ((mid * mid) <= x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+}
